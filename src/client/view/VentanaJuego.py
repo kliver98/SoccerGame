fa22d6ace862342d2caf971ffa40f01906b5b63a
Christian Andrés Cobo R <christianandrescobo@gmail.com>
2019-09-29 00:32:01 -0500

    Levantamiento de la clase: VentanaJuego y estructuración de la misma

---
+++ src/client/view/VentanaJuego.py
@@ -0,0 +1,23 @@
+#Class: MainWindow
+
+"""Esta es la clase que representa la ventana de juego(Login) de un cliente"""
+
+"""Inicio de la clase"""
+
+
+
+"""Constantes de la clase"""
+
+
+
+"""Atributos de la clase"""
+
+
+
+
+"""Metodo constructor de la clase"""
+
+
+
+"""Metodos relacionados con la clase"""
+
