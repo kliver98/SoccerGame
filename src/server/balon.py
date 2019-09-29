c7edbfa756695077c3a847bf457f8078c42a7f85
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2019-09-29 09:27:48 -0500

    creado balon.py
    creado modulo balon

---
+++ src/server/balon.py
@@ -0,0 +1,12 @@
+
+
+class Balon:
+    
+    def __init__(self):
+        self.coordenada=None
+        self.username=None
+        self.posesionA=None
+        self.posesionB=None
+        
+    def get_coordenada(self):
+        return self.coordenada
