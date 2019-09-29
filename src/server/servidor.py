0971c1b5a2a65a22932ba67811c112aac8a7b727
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2019-09-29 10:45:49 -0500

    Create servidor.py
    creada clase de servidor para escuchar peticiones y conetar clientes

---
+++ src/server/servidor.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+from server import sala
+
+MAX_JUGADORES=30
+CONEXIONES_ESPERA=10
+jugadores={}
+salas={}
+
+'''se piden los parametros para iniciar el servidor'''
+server = input("Please insert the ip server, it will be locallhost for defect")
+port = 5555
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+''' se inicia el servidor con los parametros'''
+try:
+    if server is None:
+        server="localhost"
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(CONEXIONES_ESPERA)
+print("server was started, waiting for connections")
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)#notificacion de cliente conectado, reportando su ip
+    '''  logica para crear salas, asignar jugadores a una sala'''
