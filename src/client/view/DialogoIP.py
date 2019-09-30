ea935ff263511318cc5cb53fe4692e44f1df9ef4
Christian Andrés Cobo R <christianandrescobo@gmail.com>
2019-09-30 15:29:47 -0500

    Levantamiento y estructuración de la clase: DialogoIP

---
+++ src/client/view/DialogoIP.py
@@ -0,0 +1,25 @@
+
+
+
+
+import tkinter
+
+class DialogoIP():
+    """Constantes de la clase"""
+
+    """Relaciones de la clase"""
+    
+    
+    """Atributos de la clase"""
+    
+    
+    #Ventanas de prueba
+    """Objetos tipo ventana de pruebas >>> continuar en la tarde 30/09/2019"""
+
+    
+    """Metodo constructor de la clase"""
+    
+    
+    
+    """Metodos relacionados con la clase"""
+    
