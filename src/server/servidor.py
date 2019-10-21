8685bf9b48b4660c07aef71a37111169f51f1791
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2019-10-20 20:10:38 -0500

    Agregado logica de salas
    Gestion de conexion de usuario y  asiganacion a salas disponibles y equipo correspondiente dentro de la sala

--- src/server/servidor.py
+++ src/server/servidor.py
@@ -5,12 +5,12 @@ from server import sala
 MAX_JUGADORES=30
 CONEXIONES_ESPERA=10
 jugadores={}
-salas={}
+salas=[]
 
 
 '''se piden los parametros para iniciar el servidor'''
 server = input("Please insert the ip server, it will be locallhost for defect")
-port = 5555
+port = 5558
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ''' se inicia el servidor con los parametros'''
@@ -24,11 +24,25 @@ except socket.error as e:
 s.listen(CONEXIONES_ESPERA)
 print("server was started, waiting for connections")
 
+def verificar_disponible():
+    sa=None
+    for i in salas:
+        if i.esta_disponible():
+            return i
+    return sa
 
+
+currentPlayer=0
+currentRoom=0
 while True:
     '''tupla (socket asignado, mensaje de direccion) al aceptar un cliente'''
     net, addr = s.accept() 
     print("Connected to:", addr)#notificacion de cliente conectado, reportando su ip
-    
-    
+    sa=verificar_disponible()
+    if not sa:
+        sa= sala.Sala(currentRoom)
+        salas.append(sa)
+        currentRoom+=1
+    sa.add_player(net,currentPlayer)
+    currentPlayer+=1
     '''TODO:  logica para crear salas, asignar jugadores a una sala'''
