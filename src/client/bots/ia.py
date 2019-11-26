473eff431920c605b56c43402c1d3145914a7aa2
Kliver Daniel Giron Castro <kliver.giron1@correo.icesi.edu.co>
2019-11-26 16:19:26 -0500

    Se crea red neuronal 1 que sirve para acercar a un jugador al balon

--- src/client/bots/ia.py
+++ src/client/bots/ia.py
@@ -1,12 +1,27 @@
 #librerias
 import numpy as np
-import keras
-from keras.models import Sequential
-from keras.layers.core import Dense
+from keras.models import model_from_json
+from client.bots import red_neuronal_1 as rn1
+from client.model.balon import Balon
 
-def calcular(jugadores):
+def calcular(jugadores, balon):
     #Este metodo es el encargado de actualizar las posicinoes de los jugadores pasados por parametro. No devuelve nada, actualiza los mismo objetos del model
-    pass
+    modelo = cargar_modelo()
+    if balon.get_usuario()=="": #la red 1 tiene que hacer que atrape el balon
+        if modelo==None: #Si no habia ningun modelo guardado, se crea
+            modelo = rn1.entrenar()
+            guardar_modelo(modelo)
+        #print("Prediciendo valores....")
+        #for i in range(0,101):
+        #    yy = -i/100
+        #    xx = 0
+        #    val = modelo.predict(np.array(np.array([[xx,yy]])))
+        #    speed = 1
+        #    decimals = 2
+        #    print(f"[{xx}    {yy}] -> x:{str(round((val[0][0])*speed, decimals))} - y:{str(round((val[0][1])*speed, decimals))}")
+        #print("fin prediccion de valores....")
+    else: #la red 2 tiene que hacer gol
+        pass
 
 def convertir_a_entrada(tipo,jugador):
     #de acuerdo al tipo (0 para agarrar balon, 1 para hacer gol) devuelve un arreglo en formato de  estimulo
@@ -14,5 +29,33 @@ def convertir_a_entrada(tipo,jugador):
         pass
     elif tipo==1:
         pass
-    
-print(keras.__version__)
+
+def guardar_modelo(model):
+    # serializar el modelo a JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serializar los pesos a HDF5
+    model.save_weights("model.h5")
+    print("Modelo Guardado!")
+
+def cargar_modelo():
+    # cargar json y crear el modelo
+    file_name = 'model.json'
+    try:
+        json_file = open(file_name, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # cargar pesos al nuevo modelo
+        loaded_model.load_weights("model.h5")
+        print("Cargado modelo desde disco.")
+        # Compilar modelo cargado y listo para usar.
+        loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
+    except Exception as e:
+        return None
+    return loaded_model
+
+b = Balon()
+b.set_usuario("") #Llamaria a red neuronal 1
+calcular(None,b)
---
+++ src/client/bots/red_neuronal_1.py
@@ -0,0 +1,32 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense
+
+def entrenar():
+    # datos de entreno
+    training_data = np.array([[1,-1],[1,0],[1,1],
+                          [0,-1],[0,0],[0,1],
+                          [-1,-1],[-1,0],[-1,1]],"float32")
+
+    # resultados que se obtienen, en el mismo orden
+    target_data = np.array([[-1,-1],[-1,0],[-1,1],[0,-1],[0,0],[0,1],[1,-1],[1,0],[1,1]], "float32")
+    model = Sequential()
+    model.add(Dense(16, input_dim=2, activation='tanh'))
+    model.add(Dense(2, activation='tanh'))
+
+    model.compile(loss='mean_squared_error',
+                  optimizer='adam',
+                  metrics=['binary_accuracy'])
+    
+    model.fit(training_data, target_data, epochs=2500)
+
+    # evaluamos el modelo
+    #scores = model.evaluate(training_data, target_data)
+
+    #print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    #print (model.predict(training_data).round())
+    
+    #prueba = np.array([[1,0]])
+    #print(model.predict(np.array(prueba)).round())
+    
+    return model
