ee1e5fbf04eeab5629ca8f4cb8e26b99df045a0d
Kliver Daniel Giron Castro <kliver.giron1@correo.icesi.edu.co>
2019-11-26 16:25:20 -0500

    Se agrego parametro nombre del archivo al metodo de guardar como al de cargar debido a que existiran dos modelos para dos redes neuronales

--- src/client/bots/ia.py
+++ src/client/bots/ia.py
@@ -4,13 +4,17 @@ from keras.models import model_from_json
 from client.bots import red_neuronal_1 as rn1
 from client.model.balon import Balon
 
+NOMBRE_ARCHIVO_1 = "modeloAgarrar"
+NOMBRE_ARCHIVO_2 = "modeloGol"
+
 def calcular(jugadores, balon):
     #Este metodo es el encargado de actualizar las posicinoes de los jugadores pasados por parametro. No devuelve nada, actualiza los mismo objetos del model
-    modelo = cargar_modelo()
+    modelo = None
     if balon.get_usuario()=="": #la red 1 tiene que hacer que atrape el balon
+        modelo = cargar_modelo(NOMBRE_ARCHIVO_1)
         if modelo==None: #Si no habia ningun modelo guardado, se crea
             modelo = rn1.entrenar()
-            guardar_modelo(modelo)
+            guardar_modelo(modelo,NOMBRE_ARCHIVO_1)
         #print("Prediciendo valores....")
         #for i in range(0,101):
         #    yy = -i/100
@@ -30,25 +34,25 @@ def convertir_a_entrada(tipo,jugador):
     elif tipo==1:
         pass
 
-def guardar_modelo(model):
+def guardar_modelo(model,nombre_archivo):
     # serializar el modelo a JSON
     model_json = model.to_json()
-    with open("model.json", "w") as json_file:
+    with open(f"{nombre_archivo}.json", "w") as json_file:
         json_file.write(model_json)
     # serializar los pesos a HDF5
-    model.save_weights("model.h5")
+    model.save_weights(f"{nombre_archivo}.h5")
     print("Modelo Guardado!")
 
-def cargar_modelo():
+def cargar_modelo(nombre_archivo):
     # cargar json y crear el modelo
-    file_name = 'model.json'
+    file_name = f'{nombre_archivo}.json'
     try:
         json_file = open(file_name, 'r')
         loaded_model_json = json_file.read()
         json_file.close()
         loaded_model = model_from_json(loaded_model_json)
         # cargar pesos al nuevo modelo
-        loaded_model.load_weights("model.h5")
+        loaded_model.load_weights(f"{nombre_archivo}.h5")
         print("Cargado modelo desde disco.")
         # Compilar modelo cargado y listo para usar.
         loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
